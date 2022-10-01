af629b5d4552f5522cb1cedbc12de6a8fa402be2
miliPaoletti <>
2022-10-01 09:01:55 +0000

    Update from https://github.com/miliPaoletti/ts-frontend/commit/73c82a2b537c1d6bcc1e05434cac6fb6c620093f

--- next.config.js
+++ next.config.js
@@ -8,8 +8,14 @@ const nextConfig = {
     path: "",
     domains: ["cdn.kimkim.com"],
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
+  basePath:
+    process.env.NEXT_PUBLIC_BASE_PATH === "turismoSerrano"
+      ? ""
+      : process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix:
+    process.env.NEXT_PUBLIC_BASE_PATH === "turismoSerrano"
+      ? ""
+      : process.env.NEXT_PUBLIC_BASE_PATH,
 };
 
 module.exports = nextConfig;
