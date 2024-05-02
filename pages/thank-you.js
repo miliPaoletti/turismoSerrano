ec6d1040f943560cb56a0d3bc8f14d3984e4d6d9
miliPaoletti <>
2024-05-02 11:25:04 +0000

    Update from https://github.com/miliPaoletti/ts-frontend/commit/0365db71c5d2762608afc7826f4fa0da614926b5

--- pages/thank-you.js
+++
@@ -1,8 +0,0 @@
-import Template from "components/layout/Template";
-import { ThankYou } from "components/workWithUs/ThankYou";
-import { WorkWithUs } from "components/workWithUs/workWithUs";
-import { FormTest } from "components/workWithUs/workWithUsForm";
-
-export default function ThankYouPage() {
-  return <Template content={<ThankYou />} title="Gracias por contactarnos" />;
-}
